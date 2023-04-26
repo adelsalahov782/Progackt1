ba63052ec8cd91308d9955140bf13c9260efd1e1
adelsalahov782 <127782741+adelsalahov782@users.noreply.github.com>
2023-04-26 19:24:56 +0300

    Arr

--- src/Home_Work/Str.java
+++ src/Home_Work/Str.java
@@ -37,7 +37,7 @@ public class Str {
         for (int i = 0; i < input.length; i++) {
             input[i] = scan.nextLine();
         }
-        for (int j = 0; j < input.length; j++) {
+        for (int j = 1; j < input.length; j++) {
             if (minLine.length() <= maxLine.length()) {
                 minLine = input[j];
             }
